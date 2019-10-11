ad1458f824c083547ec2209819e72a5a7eb5c3e6
maly7 <paul.aly@outlook.com>
2019-10-10 22:37:58 -0400

    configure application security

---
+++ src/main/kotlin/com/lumberjackdev/sso/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.lumberjackdev.sso
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http!!.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
